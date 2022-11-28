e9dc51fe6c20bdc94a6bc3a38c223ee57b56b4cd
agaliamor <d.shkarban01@gmail.com>
2022-11-28 12:35:45 +0500

    commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// задание 1 
+// for (i = 1; i <= 100; i++) {
+// 	document.write(i + '<br>');
+// } 
+
+// задание 2 
+// for (i = 100; i >= 1; i--) {
+//   document.write(i + '<br>');
+// } 
+
+// задание 3
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     document.write(i + '<br>'); 
+//   }
+// }
+
+
+var arr = [];
+for(i = 1; i <= 10; i++) {
+  const a = [11, 12, 13]; 
+  console.log(a); 
+  document.write(arr);
+}
+
+
